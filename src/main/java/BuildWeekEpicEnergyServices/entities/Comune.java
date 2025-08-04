6cde45a7dcd068433ee3bb676bdfc039360da50d
Aledigio96 <alessandro.digiovanni10@gmail.com>
2025-08-04 14:25:16 +0200

    import file.csv (apache.commons)

---
+++ src/main/java/BuildWeekEpicEnergyServices/entities/Comune.java
@@ -0,0 +1,29 @@
+package BuildWeekEpicEnergyServices.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "comuni")
+@Getter
+@Setter
+public class Comune {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "codice_provincia")
+    private String codiceProvincia;
+
+    @Column(name = "progressivo_comune")
+    private String progressivoComune;
+
+    @Column(name = "denominazione_comune")
+    private String denominazioneComune;
+
+    @Column(name = "provincia")
+    private String provincia;
+
+
+}
