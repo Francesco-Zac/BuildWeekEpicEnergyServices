c5568dd6060232eb11e385cb0550893fd4f0747a
Borislav Kozhuharski <nomercyforyouss@gmail.com>
2025-08-04 14:34:56 +0200

    Creato l'entità di Fattura

---
+++ src/main/java/BuildWeekEpicEnergyServices/Fattura.java
@@ -0,0 +1,33 @@
+package BuildWeekEpicEnergyServices;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "fatture")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Fattura {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(name = "cliente_id", nullable = false)
+    private UUID clienteId;
+
+    @Column(name = "stato_id", nullable = false)
+    private UUID statoId;
+
+    @Column(nullable = false)
+    private LocalDate data;
+
+    @Column(nullable = false)
+    private String numero;
+}
