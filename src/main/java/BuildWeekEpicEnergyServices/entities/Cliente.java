935d9c4b4e8a8baba91837fa0c759fa8ac80ad7a
Aledigio96 <alessandro.digiovanni10@gmail.com>
2025-08-04 18:01:50 +0200

    change cliente and fattura

--- src/main/java/BuildWeekEpicEnergyServices/entities/Cliente.java
+++ src/main/java/BuildWeekEpicEnergyServices/entities/Cliente.java
@@ -5,6 +5,8 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "clienti")
@@ -66,6 +68,9 @@ public class Cliente {
     @OneToOne
     @JoinColumn(name = "sede_legale")
     private Indirizzo sedeLegale;
+
+    @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
+    private List<Fattura> fatture = new ArrayList<>();
 
     public Cliente(String ragioneSociale, long partitaIva, String email, LocalDate inseritoIl, LocalDate ultimoContattoIl, double fatturatoAnnuo, String pec, String numeroTelefono, String emailContatto, String nomeContatto, String cognomeContatto, String telefonoContatto, String logoAzienda, TipoAzienda tipo, Indirizzo sedeOperativa, Indirizzo sedeLegale) {
         this.ragioneSociale = ragioneSociale;
