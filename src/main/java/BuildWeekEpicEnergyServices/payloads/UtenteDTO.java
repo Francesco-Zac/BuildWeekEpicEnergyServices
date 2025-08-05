1c43fd1467f3258e2cd70526308edf2c670af492
Francesco-Zac <francescozaccaro2@gmail.com>
2025-08-05 12:10:09 +0200

    change class to record

--- src/main/java/BuildWeekEpicEnergyServices/payloads/UtenteDTO.java
+++ src/main/java/BuildWeekEpicEnergyServices/payloads/UtenteDTO.java
@@ -4,81 +4,12 @@ import jakarta.validation.constraints.*;
 
 import java.util.Set;
 
-public class UtenteDTO {
-    @NotBlank
-    private String username;
-
-    @NotBlank
-    @Email
-    private String email;
-
-    @NotBlank
-    private String password;
-
-    @NotBlank
-    private String nome;
-
-    @NotBlank
-    private String cognome;
-
-    private String avatar;
-
-    private Set<Long> ruoliIds;
-
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCognome() {
-        return cognome;
-    }
-
-    public void setCognome(String cognome) {
-        this.cognome = cognome;
-    }
-
-    public String getAvatar() {
-        return avatar;
-    }
-
-    public void setAvatar(String avatar) {
-        this.avatar = avatar;
-    }
-
-    public Set<Long> getRuoliIds() {
-        return ruoliIds;
-    }
-
-    public void setRuoliIds(Set<Long> ruoliIds) {
-        this.ruoliIds = ruoliIds;
-    }
-}
+public record UtenteDTO(
+        @NotBlank String username,
+        @NotBlank @Email String email,
+        @NotBlank String password,
+        @NotBlank String nome,
+        @NotBlank String cognome,
+        String avatar,
+        Set<Long> ruoliIds
+) {}
