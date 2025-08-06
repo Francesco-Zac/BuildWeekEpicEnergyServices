abd0829fa1c4e39f7f241b61980faa923362a35d
Borislav Kozhuharski <nomercyforyouss@gmail.com>
2025-08-06 08:08:54 +0200

    Sistemato errore Cliente, e Service

--- src/main/java/BuildWeekEpicEnergyServices/services/FatturaService.java
+++ src/main/java/BuildWeekEpicEnergyServices/services/FatturaService.java
@@ -14,11 +14,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
-import java.awt.print.Pageable;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
